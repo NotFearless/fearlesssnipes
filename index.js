c3ce6d90dd7d1a1a44543c6de3bc7849918e8224
Nolan Decker <nolanisboss2001@gmail.com>
2019-01-04 22:22:17 -0600

    Handle login rejection

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
 
+// require packages
 const Discord = require('discord.js');
 const settings = require('./settings.json');
 const fs = require('fs');
-require('dotenv/config');
-const http = require('http');
-const port = process.env.PORT || 3000;
 
-http.createServer().listen(port);
- 
-
+// initialise are bot
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
- 
 
+// import bot setting (data)
 const prefix = settings.prefix;
-const token = process.env.TOKEN;
+const token = settings.token;
 const owner = settings.owner;
- 
 
+//read commands files
 fs.readdir('./cmds', (err,files) => {
     if (err) {
         console.log(err);
     }
- 
+
     let cmdFiles = files.filter(f => f.split(".").pop() === "js");
- 
+
     if (cmdFiles.length === 0){
         console.log("No files found");
         return;
     }
- 
+
     cmdFiles.forEach((f,i) => {
         let props = require(`./cmds/${f}`);
         console.log(`${i+1}: ${f} loaded`);
         bot.commands.set(props.help.name, props);
     })
 })
- 
+
 let raw = fs.readFileSync('./roles.json');
 let allowedRoles = JSON.parse(raw);
- 
+
 let validation = function(serverRoles, userRoles){
     let val = false;
     serverRoles.forEach((role) => {
@@ -51,26 +47,26 @@ let validation = function(serverRoles, userRoles){
     });
     return val;
 }
- 
- 
- 
- 
+
+
+
+
 bot.on('ready', async () => {
     console.log("Hello, im ready");
- 
+
 });
- 
- 
+
+
 bot.on('message',msg => {
     if (msg.channel.type === "dm") return;
     if (msg.author.bot) return;
- 
+
     let msg_array = msg.content.split(" ");
     let command = msg_array[0];
     let args = msg_array.slice(1);
- 
+
     if (!command.startsWith(prefix)) return;
- 
+
     if (bot.commands.get(command.slice(prefix.length))){
         if (validation(allowedRoles.roles,msg.member.roles.array()) || msg.member.id === owner){
                 let cmd = bot.commands.get(command.slice(prefix.length));
@@ -78,16 +74,12 @@ bot.on('message',msg => {
                     cmd.run(bot,msg,args);
                 }
         } else {
-                msg.channel.send("You dont have access to this bot commands");
+                msg.channel.send("You dont have acces to this bot commands");
         }
 }
-   
+    
 });
- 
-bot.on('error', err => {
-    console.log(err);
-});
- 
- 
 
-bot.login(token).catch(console.log);
+
+// Bot login
+bot.login(token);
